7acd574dd42feca929b765a8d136a998c3eb962d
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-01-24 14:41:51 -0300

    refactor: extract table row into a separate OrderTableRow component

--- src/pages/app/orders/index.tsx
+++ src/pages/app/orders/index.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { ArrowRight, Search, X } from 'lucide-react'
+import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Helmet } from 'react-helmet-async'
+import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
   return(
@@ -32,44 +31,7 @@ export function Orders() {
             </TableHeader>
             <TableBody>
               {Array.from({ length: 10 }).map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell>
-                    <Button variant='outline' size='xs'>
-                      <Search className='h-3 w-3'/>
-                      <span className='sr-only'>Detalhes do pedido</span>
-                    </Button>
-                  </TableCell>
-                  <TableCell className='font-mono text-sm font-medium'>
-                    sdfsdf4fs45df46dfs46
-                  </TableCell>
-                  <TableCell className='text-muted-foreground'>
-                    há 15 mnts
-                  </TableCell>
-                  <TableCell>
-                    <div className='flex items-center gap-2'>
-                      <span className='h-2 w-2 rounded-full bg-slate-400' />
-                      <span className='font-medium text-muted-foreground'>Pendente</span>
-                    </div>
-                  </TableCell>
-                  <TableCell className='font-medium'>
-                    Fau Santos
-                  </TableCell>
-                  <TableCell className='font-medium'>
-                    R$ 149,90
-                  </TableCell>
-                  <TableCell>
-                    <Button variant='outline' size='xs'>
-                      <ArrowRight className='h-3 w-3 mr-2'/>
-                      Aprovar
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button variant='ghost' size='xs'>
-                      <X className='h-3 w-3 mr-2'/>
-                      Cancelar
-                    </Button>
-                  </TableCell>
-                </TableRow>
+                <OrderTableRow key={index}/>
               ))}
             </TableBody>
           </Table>
