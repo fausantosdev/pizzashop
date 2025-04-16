7468d6981b96a07461dde0b1a2843e45fc35ae3c
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-04-16 20:37:50 -0300

    feat: add custom 404 error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { SignUp } from './pages/auth/sign-up'
 import { SignIn } from './pages/auth/sign-in'
 import { Orders } from './pages/app/orders'
 import { Dashboard } from './pages/app/dashboard'
+import NotFound from './pages/404'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout/>,
+    errorElement: <NotFound/>,
     children: [
       {
         path: '/',
