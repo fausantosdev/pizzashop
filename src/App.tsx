bda08380151563cb87da6be74f0aed56d8eb4956
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-08-11 10:22:58 -0300

    refactor: reorder QueryClientProvider import for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 
 import { router } from './routes'
-import { QueryClientProvider } from '@tanstack/react-query'
+
 import { queryClient } from './lib/react-query'
 
 export function App() {
